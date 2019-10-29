f1566769f4621e6e00787cb141781006275aa2ac
Mark Dietzler <mdietzler@eagles.ewu.edu>
2019-10-28 17:02:27 -0700

    created solution for assignment 4

---
+++ Configuration/EnvironmentConfig.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Configuration
+{
+    public class EnvironmentConfig : IConfig
+    {
+
+        private List<string> keyName;
+
+        public EnvironmentConfig()
+        {
+            keyName = new List<string>();
+        }
+        public bool GetConfigValue(string name, string? value)
+        {
+            value = Environment.GetEnvironmentVariable(name);
+            if (value == null)
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        public bool SetConfigValue(string name, string? value)
+        {
+            Environment.SetEnvironmentVariable(name, value);
+            if (value == null)
+            {
+                return false;
+            }
+            else
+            {
+                keyName.Add(name);
+                return true;
+            }
+        }
+
+        ~EnvironmentConfig()
+        {
+            foreach (string name in keyName)
+                Environment.SetEnvironmentVariable(name, null);
+        }
+    }
+}
---
+++ Configuration/FileConfig.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Configuration
+{
+    public class FileConfig
+    {
+        private string path;
+
+        public FileConfig(string newPath)
+        {
+            this.path = newPath;
+        }
+
+        public List<string> ReadConfig()
+        {
+            string[] readLine = File.ReadAllLines(path);
+
+            List<string> res = new List<string>();
+
+            foreach (string s in readLine)
+            {
+                if (s.Split("=").Length != 2)
+                    throw new ArgumentException("must have two args");
+
+                res.Add(s);
+            }
+
+            return res;
+        }
+
+#pragma warning disable CA1822 // Mark members as static
+        public void WriteConfig(string name, string? value)
+#pragma warning restore CA1822 // Mark members as static
+        {
+            if (name != null || value != null || !value.Contains(" ") || !value.Contains("=") || !name.Contains(" ") || !name.Contains("="))
+            {
+                using (StreamWriter streamWriter = new StreamWriter(path, true))
+                {
+                    streamWriter.WriteLine($"{name}={value}");
+                }
+            }
+
+            else Console.WriteLine("error to write the content");
+        }
+
+
+    }
+}
