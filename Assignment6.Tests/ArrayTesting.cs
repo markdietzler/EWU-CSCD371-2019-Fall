f499031fd24c31d0fa70246c3359630e24ae8cd7
markdietzler <mdietzler@eagles.ewu.edu>
2019-11-12 06:46:37 -0800

    Updated tests, split one test, and added more tests

--- Assignment6.Tests/ArrayTesting.cs
+++ Assignment6.Tests/ArrayTesting.cs
@@ -7,21 +7,26 @@ namespace Assignment6.Tests
     [TestClass]
     public class ArrayTesting
     {
-        [TestMethod]        
+        [DataTestMethod]
+        [DataRow(1)]
+        [DataRow(2)]
+        [DataRow(3)]
         [ExpectedException(typeof(IndexOutOfRangeException))]
-        public void Array_Add_At_OutOfBounds_Index()
+        public void Add_AtOutOfBoundsIndex_ThrowsException(int addMe)
         {
-            var _TestArray1 = new Array<int>();
-            _TestArray1.Add(1);
+            var _TestArray = new Array<int>
+            {
+                1,2,3
+            };
 
-            _TestArray1[4] = 4;
+            _TestArray[4] = 4;
         }        
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentOutOfRangeException))]
-        public void Array_Of_Negative_Size_Throws_Exception()
+        public void Constructor_NegativeSize_ThrowsException()
         {
-            var _TestArray3 = new Array<int>(-1);
+            var _TestArray = new Array<int>(-1);
         }
 
         [DataTestMethod]
@@ -29,7 +34,7 @@ namespace Assignment6.Tests
         [DataRow(2)]
         [DataRow(3)]
         [ExpectedException(typeof(ArgumentOutOfRangeException))]
-        public void Array_Add_To_Full_Array_Throws_Exception(int addMe)
+        public void Add_AddToFullArray_Throws_Exception(int addMe)
         {
             var _TestArray = new Array<int>(3)
             {
@@ -43,7 +48,7 @@ namespace Assignment6.Tests
         [DataRow(1)]
         [DataRow(2)]
         [DataRow(3)]
-        public void Array_Add_Success(int addMe)
+        public void Add_OneItem_Success(int addMe)
         {
             var _TestArray = new Array<int>
             {
@@ -52,12 +57,25 @@ namespace Assignment6.Tests
 
             _TestArray.Add(4);
 
-            Assert.AreEqual(_TestArray.Count, 4);
-            Assert.AreEqual(_TestArray[3], 4);
+            Assert.AreEqual(_TestArray.Count, 4);            
+        }
+
+        [DataTestMethod]
+        [DataRow(1)]
+        [DataRow(2)]
+        [DataRow(3)]
+        public void ArrayIndex_EqualsValue_Success(int addMe)
+        {
+            var _TestArray = new Array<int>
+            {
+                1,2,3
+            };
+
+            Assert.AreEqual(_TestArray[2], 3);
         }
 
         [TestMethod]
-        public void Array_Clear_Array_Success()
+        public void ClearArray_Success()
         {
             var _TestArray = new Array<int>
             {
@@ -73,7 +91,7 @@ namespace Assignment6.Tests
         [DataRow(1)]
         [DataRow(2)]
         [DataRow(3)]
-        public void Array_Contains_Success(int item)
+        public void Contains_KnownGoodValue_Success(int item)
         {
             var _TestArray = new Array<int>
             {
@@ -86,7 +104,7 @@ namespace Assignment6.Tests
         }
 
         [TestMethod]
-        public void Array_CopyTo_Success()
+        public void CopyTo_NewArray_Success()
         {
             var _TestArray = new Array<int>
             {
@@ -104,7 +122,7 @@ namespace Assignment6.Tests
         [DataRow(1)]
         [DataRow(2)]
         [DataRow(3)]
-        public void Array_Remove_Success(int removeMe)
+        public void Remove_KnownPresentItem_SuccessViaContains(int removeMe)
         {
             var _TestArray = new Array<int>
             {
@@ -114,6 +132,39 @@ namespace Assignment6.Tests
             _TestArray.Remove(removeMe);
 
             Assert.IsFalse(_TestArray.Contains(removeMe));
+        }
+
+        [DataTestMethod]
+        [DataRow(1)]
+        [DataRow(2)]
+        [DataRow(3)]
+        public void Remove_KnownPresentItem_SuccessViaTrueBoolean(int addMe)
+        {
+            var _TestArray = new Array<int>
+            {
+                1,2,3
+            };
+
+            bool successfulDelete = _TestArray.Remove(3);
+
+            Assert.IsTrue(successfulDelete);
+
+        }
+
+        [DataTestMethod]
+        [DataRow(1)]
+        [DataRow(2)]
+        [DataRow(3)]
+        public void Remove_KnownNotPresentItem_SuccessViaFalseBoolean(int addMe)
+        {
+            var _TestArray = new Array<int>
+            {
+                1,2,3
+            };
+
+            bool unsuccessfulDelete = _TestArray.Remove(4);
+
+            Assert.IsFalse(unsuccessfulDelete);
         }
 
     }
