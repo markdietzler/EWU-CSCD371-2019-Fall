d8929cf9e89cd09395c4de33c8541c7bd5188593
Mark Dietzler <mdietzler@eagles.ewu.edu>
2019-11-18 18:14:15 -0800

    Finalized solution and tests

--- DelegateFun/Sorter.Tests/SortUtilityTests.cs
+++ DelegateFun/Sorter.Tests/SortUtilityTests.cs
@@ -1,4 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using static Sorter.SortUtility;
 
 namespace Sorter.Tests
 {
@@ -6,8 +8,85 @@ namespace Sorter.Tests
     public class SortUtilityTests
     {
         [TestMethod]
-        public void SortUtility_ShouldSortAscending_UsingAnAnonymousMethod()
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SortUtilNullArrayNullDelagateThrowsException()
         {
+            //Arrange
+            SortUtility testUtil = new SortUtility();
+
+            //Act
+            testUtil.SelectionSortUtil(null!, null!);
+            //Assert
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SortUtilNullDelagateThrowsException()
+        {
+            //Arrange
+            int[] rawArray = new int[] { 7,2,0 };
+            SortUtility testUtil = new SortUtility();
+
+            //Act
+
+            testUtil.SelectionSortUtil(rawArray, null!);
+
+            //Assert
+        }
+
+        [TestMethod]
+        public void SortUtilkAortAscendingOrderWithAnonymous()
+        {
+            //Arrange
+            int[] rawArray = new int[] {6,0,1,5,2,4};
+            int[] sorted = new int[] {0,1,2,4,5,6};
+            SortUtility util = new SortUtility();
+
+            //Act
+            AnalyzeInts analyze = delegate (int thisOne, int thatOne)
+            {
+                return thisOne < thatOne;
+            };
+
+            rawArray = util.SelectionSortUtil(rawArray, analyze);
+
+            //Assert
+            CollectionAssert.AreEqual(rawArray, sorted);
+        }
+
+        [TestMethod]
+        public void SortUtilSortDescendingLamda()
+        {
+            //Arrange
+            int[] rawArray = new int[] { 9, 66, 32, 84, 101, 57, 42, 1 };
+            int[] sorted = new int[] { 101, 84, 66, 57, 42, 32, 9, 1 };
+            SortUtility util = new SortUtility();
+
+            //Act
+
+            rawArray = util.SelectionSortUtil(rawArray, ((int thisOne, int thatOne) => thisOne > thatOne));
+
+            //Assert
+            CollectionAssert.AreEqual(rawArray, sorted);
+        }
+
+        [TestMethod]
+        public void SortUtilSortAscendingLabdaStatement()
+        {
+            //Arrange
+            int[] rawArray = new int[] { 19, 1031, 721, 33, 998, 211, 88, 599, 151, 463 };
+            int[] sortedAscending = new int[] { 19, 33, 88, 151, 211, 463, 599, 721, 998, 1031 };
+            SortUtility util = new SortUtility();
+
+            //Act
+            rawArray = util.SelectionSortUtil(rawArray, (thisOne, thatOne) =>
+            {
+                return thisOne < thatOne;
+            });
+
+            //Assert
+            CollectionAssert.AreEqual(rawArray, sortedAscending);
         }
     }
 }
--- DelegateFun/Sorter/SortUtility.cs
+++ DelegateFun/Sorter/SortUtility.cs
@@ -4,7 +4,42 @@ namespace Sorter
 {
     public class SortUtility
     {
-        // Sort method should be implemented here
-        // It should accept an int[] and a delegate you define that performs the actual comparison
+        public delegate bool AnalyzeInts(int thisOne, int thatOne);
+
+        /// <summary>
+        /// Standard insertion sort for int arrays
+        /// </summary>
+        /// <param name="rawArrayToSort">Array to sort</param>
+        /// <param name="analyze">Delegate taking the place of compareTo</param>
+        /// <returns>Sorted array</returns>
+        public int[] SelectionSortUtil(int[] rawArrayToSort, AnalyzeInts analyze)
+        {
+            if(rawArrayToSort is null || analyze is null)
+            {
+                throw new ArgumentNullException(nameof(rawArrayToSort));
+            }
+
+            for (int start = 0; start < rawArrayToSort.Length -1 ; start ++)
+            {
+                int low = start;
+                for(int search = start + 1; search < rawArrayToSort.Length; search++)
+                {
+                    if(analyze(rawArrayToSort[search], rawArrayToSort[low]))
+                    {
+                        low = search;
+                    }
+
+                } //end of inner for
+
+                //do the swapping-pokey, and move them all around
+                int swap = rawArrayToSort[low];
+                rawArrayToSort[low] = rawArrayToSort[start];
+                rawArrayToSort[start] = swap;
+
+            } //end of outer for
+
+            return rawArrayToSort;
+        }
+
     }
 }
