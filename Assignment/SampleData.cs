ee7823e7d86c2828bc01d4204e552ffdbd5767c7
Mark Michaelis <mark@IntelliTect.com>
2019-11-21 19:39:07 -0800

    Sample code on 'More LINQ' from class today.

--- Assignment/SampleData.cs
+++ Assignment/SampleData.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
 
 namespace Assignment
 {
     public class SampleData : ISampleData
     {
+        public enum Column
+        {
+            Id, FirstName, LastName, Email, StreetAddress, City, State, Zip
+        }
+
+
         // 1.
         public IEnumerable<string> CsvRows => throw new NotImplementedException();
 
@@ -26,5 +35,44 @@ namespace Assignment
         // 6.
         public string GetAggregateListOfStatesGivenPeopleCollection(
             IEnumerable<IPerson> people) => throw new NotImplementedException();
+
+
+        public IEnumerable<(string, IEnumerable<Address>)> SelectStateWithChildAddresses()
+        {
+            List<string> headers = null;
+            IEnumerable<Address> addresses = File.ReadAllLines("People.csv")
+                .Where(item =>
+                {
+                    if (headers is null)
+                    {
+                        headers = item.Split(',').ToList();
+                        return false;
+                    }
+                    return !string.IsNullOrWhiteSpace(item);
+                })
+                .Select(item =>
+                {
+
+                    string[] columns = item.Split(',');
+                    return new Address()
+                    {
+                        StreetAddress = columns[headers.IndexOf("StreetAddress")],
+                        City = columns[(int)Column.City],
+                        State = columns[(int)Column.State],
+                        Zip = columns[(int)Column.Zip],
+                    };
+                }).OrderBy(item=>item.State);
+
+            // Leveraging Query Expression
+            IEnumerable<IGrouping<string, Address>> stateWithAddress =
+                from item in addresses group item by item.State;
+
+            // Using Standard Query Operators
+            stateWithAddress =
+                addresses.GroupBy(item => item.State);
+
+            return stateWithAddress.Select(item =>
+                (item.Key, (IEnumerable<Address>)item));
+        }
     }
 }
---
+++ AssignmentTests/SampleDataTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using static Assignment.SampleData;
+
+namespace Assignment.Tests
+{
+    [TestClass]
+    public class SampleDataTests
+    {
+        [TestMethod]
+        public void SelectStateWithChildAddressesTest()
+        {
+            SampleData data = new SampleData();
+            List<(string State, IEnumerable<Address> Addresses)> actual = data.SelectStateWithChildAddresses().ToList();
+
+            IEnumerable<string> lines = File.ReadAllLines("People.csv")
+                .Skip(1)
+                .Where(item => !string.IsNullOrWhiteSpace(item)).ToList();
+
+            Assert.AreEqual<int>(
+                lines.Select(line => line.Split(',')[(int)Column.State])
+                .Distinct()
+                .Count(),
+                actual.Count());
+
+            Assert.AreEqual<int>(
+                lines.Count(),
+                actual.Sum(item => item.Addresses.Count()));
+
+            Assert.IsTrue(actual.All(item =>
+                item.Addresses.All(address =>
+                    address.State == item.State)));
+        }
+    }
+}
