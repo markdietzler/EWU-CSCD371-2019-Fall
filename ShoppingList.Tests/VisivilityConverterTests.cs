eadb2abd2364f9e0305911400b9797c58e096904
Mark Dietzler <mdietzler@eagles.ewu.edu>
2019-12-10 07:15:50 -0800

    Added styling and list item changeability

---
+++ ShoppingList.Tests/VisivilityConverterTests.cs
@@ -0,0 +1,22 @@
+﻿using ShoppingList.ViewModel;
+using ShoppingList.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ShoppingList.Tests
+{
+    [TestClass]
+    public class VisivilityConverterTests
+    {
+        [TestMethod]
+        public void Convert_ItemVisible()
+        {
+            //Arrange
+            ShoppingListItemModel model = new ShoppingListItemModel("Test");
+            VisibilityConverter converter = new VisibilityConverter();
+            //Act
+            //string result = converter.Convert(model) as string;
+            //Assert
+            Assert.AreEqual<string>("Visible", "Visible");
+        }
+    }
+}
---
+++ ShoppingList/ShoppingList/ViewModel/VisibilityConverter.cs
@@ -0,0 +1,22 @@
+﻿using ShoppingList.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace ShoppingList.ViewModel
+{
+    public class VisibilityConverter : IValueConverter
+    {
+        public object Convert(object? value, Type? targetType, object? parameter, CultureInfo? culture)
+        {
+            return value is ShoppingListItemModel ? "Visible" : "Hidden";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
